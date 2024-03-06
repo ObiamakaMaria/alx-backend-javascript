c617fb6f9e504c726ed26c5508998673d90fdad8
Obiamaka Maria <obiamaka0101@gmail.com>
2024-03-06 23:45:25 +0000

    FIXED SOME ERRORS IN ADVANCED TASKS

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const myNewObject = report.allEmployees;
   const myNewArray = [];
 
-  for (const idx in newObject) {
+  for (const idx in myNewObject) {
     if (idx) {
       myNewArray.push(...myNewObject[idx]);
     }
