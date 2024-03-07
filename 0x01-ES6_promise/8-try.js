e1b141fcb74883d0aa02cb7a2f76693bdb85083a
Obiamaka Maria <obiamaka0101@gmail.com>
2024-03-07 00:40:33 +0000

    Javascript Promises Mandatory 6, 7 and 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -3,5 +3,5 @@ export default function divideFunction(numerator, denominator) {
     throw new Error('cannot divide by 0');
   }
 
-  return numerator / denominator;
+  return (numerator / denominator);
 }
