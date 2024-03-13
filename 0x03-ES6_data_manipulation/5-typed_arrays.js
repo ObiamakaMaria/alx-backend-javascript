20877463b687a43be359e423e7fa5f989b26fd88
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-13 03:15:34 +0000

    0x03. ES6 data manipulation Mandatory task 1 - 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,10 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new RangeError('Position outside range');
   }
-  const arrayBuffer = new ArrayBuffer(length);
-  const arrayInt8 = new Int8Array(arrayBuffer, 0, length);
+  const BufferArray = new ArrayBuffer(length);
+  const Int8arr = new Int8Array(BufferArray, 0, length);
 
-  arrayInt8.set([value], position);
+  Int8arr.set([value], position);
 
-  return new DataView(arrayBuffer);
+  return new DataView(BufferArray);
 }
