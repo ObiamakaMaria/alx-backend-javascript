0562eb27313b17c2353c402976d900583d2dfee1
Obiamaka Aghadiuno <125522368+ObiamakaMaria@users.noreply.github.com>
2024-05-01 03:42:42 +0100

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+/**
+ * Counts the number of students and their details from a database file.
+ * @param {string} path - The path to the database file.
+ * @throws {Error} Throws an error if the database file cannot be loaded.
+ */
+function countStudents(path) {
+    try {
+      const info = fs.readFileSync(path, { encoding: 'utf8' });
+      const lines = info.split('\n').filter((line) => line.length > 0);
+  
+      console.log(`Number of students: ${lines.length - 1}`);
+  
+      const fields = {};
+      let isFirstLine = true;
+      for (const line of lines) {
+        if (isFirstLine) {
+          isFirstLine = false;
+          continue; // eslint-disable 
+        }
+        const student = line.split(',');
+        if (!fields[student[3]]) {
+          fields[student[3]] = [];
+        }
+        if (student[0] !== 'firstname' && student[3]) {
+          fields[student[3]].push(student[0]);
+        }
+      }
+      for (const field in fields) {
+        if (field) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+      }
+    } catch (error) {
+      throw new Error('Cannot load the database');
+    }
+  }
+  
+  module.exports = countStudents;
